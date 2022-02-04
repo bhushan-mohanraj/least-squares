ec1cb6d94974e01207bad5ee1327da462d39a422
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2022-02-04 03:29:58 -0500

    Remove unneeded variables.

--- app/variables.py
+++ app/variables.py
@@ -5,21 +5,10 @@ Variables used throughout the app.
 import tkinter as tk
 
 
-# The slope and intercept of the optimal line
-# that minimizes the sum of squared residuals.
-optimal_m = tk.DoubleVar()
-optimal_b = tk.DoubleVar()
-
-# The sum of squared residuals for the optimal line.
-optimal_area = tk.DoubleVar()
-
 # The slope and intercept of the approximation line
 # that the user sets through the app settings.
 approximation_m = tk.DoubleVar()
 approximation_b = tk.DoubleVar()
-
-# The sum of squared residuals for the approximation line.
-approximation_area = tk.DoubleVar()
 
 # Display the squares for each residual.
 display_residual_squares = tk.BooleanVar()
