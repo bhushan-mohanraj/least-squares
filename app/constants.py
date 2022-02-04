6797abc8fc61a3442c7a8f41e295f7de8bef7531
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2022-02-04 00:16:43 -0500

    Remove unneeded constants.

--- app/constants.py
+++ app/constants.py
@@ -5,7 +5,3 @@ Constants used throughout the app.
 # The vertical and horizontal padding for many elements.
 PADDING_X = 6
 PADDING_Y = 6
-
-# The fonts used for most text.
-LARGE_FONT = ("Helvetica", 36)
-SMALL_FONT = ("Helvetica", 18)
